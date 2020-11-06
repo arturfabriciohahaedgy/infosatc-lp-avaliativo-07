9c499ac51e957aaefc363aa59bd2319ac30ef9a1
arturfabriciohahaedgy <arturfabricio99@gmail.com>
2020-11-06 17:04:57 -0300

    feat: exericico 5 concluido.

---
+++ exercicio05.py
@@ -0,0 +1,5 @@
+#funcao = lambda y: return y 
+#h = funcao(4) 
+#print(h)
+
+print("O resultado da como erro na sintaxe, isso ocorre pois não é possivel utilizar o comando 'return' em uma função Lambda.")
