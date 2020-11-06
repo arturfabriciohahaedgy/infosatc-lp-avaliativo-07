1311784348ad9db47896b186e8f3e6af0bfefb17
arturfabriciohahaedgy <arturfabricio99@gmail.com>
2020-11-06 17:04:22 -0300

    feat: exercicio 2 concluido.

---
+++ exercicio02.py
@@ -0,0 +1,4 @@
+numeroUm = int(input("Insira um número para a multiplicação: "))
+numeroDois = int(input("Insira outro número para a multiplicação: "))
+multiplicacao = lambda numeroPrimeiro, numeroSegundo: print("Multiplicação:", numeroPrimeiro * numeroSegundo)
+multiplicacao(numeroUm, numeroDois)
